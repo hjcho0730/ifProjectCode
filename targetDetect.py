4eef7da42ff1b1e85ede7145e711f982bc3214c0
hjcho0730 <111036523+hjcho0730@users.noreply.github.com>
2026-09-07 22:49:12 +0900

    길찾기 코드 반영함

--- targetDetect.py
+++ targetDetect.py
@@ -2,13 +2,21 @@ import cv2
 import os
 import mediapipe as mp
 
+import numpy as np
+
 from matrixCal import *
+from lineMade import *
 
 current_path = os.getcwd()
 file_path = os.path.join(current_path, "my_list.txt")
 
+# 1. 아루코 사전 및 검출기 설정
+dictionary_id=cv2.aruco.DICT_4X4_50
+aruco_dict = cv2.aruco.getPredefinedDictionary(dictionary_id)
+parameters = cv2.aruco.DetectorParameters()
+detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
 def get_single_marker_corners_list(
-    image, target_id=0, dictionary_id=cv2.aruco.DICT_4X4_50
+    image, target_id=0,
 ):
   """특정 ID의 아루코 마커 하나를 검출하고 4개의 모서리 픽셀 좌표를 파이썬 리스트로 반환하는 함수
 
@@ -18,10 +26,7 @@ def get_single_marker_corners_list(
   """
   debug_image = image.copy()
 
-  # 1. 아루코 사전 및 검출기 설정
-  aruco_dict = cv2.aruco.getPredefinedDictionary(dictionary_id)
-  parameters = cv2.aruco.DetectorParameters()
-  detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
+  
 
   # 2. 마커 검출
   corners, ids, rejected = detector.detectMarkers(image)
@@ -91,8 +96,10 @@ mp_pose = mp.solutions.pose
 # 웹캠을 열어 실시간으로 영상을 가져옵니다.
 cap = cv2.VideoCapture(0)
 
-matrix= None
-matrix_inv= None
+matrix= None  # 2D -> 3D
+matrix_inv= None  # 3D -> 2D
+bodyPoints= [] #2D 스크린 좌표
+route= [] #예정 경로
 # 포즈 모델 사용
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
@@ -101,29 +108,30 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             print("카메라로부터 영상을 가져올 수 없습니다.")
             continue
         suc, markPts, dImage = get_single_marker_corners_list(frame, target_id=0)
-        #frame= cv2.flip(frame, 1)
         frame= dImage
-        # BGR 이미지를 RGB로 변환
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        
+
         h,w,_= frame.shape
-        
-        # 프레임을 포즈 모델에 전달하여 포즈를 처리
-        results = pose.process(frame)
-        
-        # RGB 이미지를 다시 BGR로 변환하여 OpenCV에서 사용
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        
+
+        #frame= cv2.flip(frame, 1)
+
+        # BGR 이미지를 RGB로 변환
+        results = pose.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)) # 프레임을 포즈 모델에 전달하여 포즈를 처리
+
+        ########### 변환 행렬 계산 ###################
         if suc:
             frame= draw_dots(frame, markPts)
             if len(markPts) == 4:
                 matrix= getMatrix4(markPts, R)
-                matrix_inv = np.linalg.inv(matrix)
+                try:
+                    matrix_inv = np.linalg.inv(matrix)
+                except Exception as e:
+                    print(f"Error: {e}")
+        ############################################
+
                 
         # 포즈 랜드마크가 감지되면 랜드마크와 연결선 그리기
         blue_pts= []
         if results.pose_landmarks:
-            print(type(results.pose_landmarks))
             mp_drawing.draw_landmarks(
                 image=frame,
                 landmark_list=results.pose_landmarks,
@@ -131,59 +139,57 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                 landmark_drawing_spec=mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2),
                 connection_drawing_spec=mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2)
             )
-            print()
-            with open(file_path, "w", encoding="utf-8") as f:
-                for item in results.pose_landmarks.landmark:
-                    f.write(f"{item}\n")
-            print(f"파일이 다음 경로에 저장되었습니다: {file_path}")
+
+            ####### 신체 형상 데이터 -> 파일 저장 ############
+            # print()
+            # with open(file_path, "w", encoding="utf-8") as f:
+            #     for item in results.pose_landmarks.landmark:
+            #         f.write(f"{item}\n")
+            # print(f"파일이 다음 경로에 저장되었습니다: {file_path}")
+            ###############포즈 모델 값 저장##################
             f= (lambda idx: (results.pose_landmarks.landmark[idx].x * w, results.pose_landmarks.landmark[idx].y * h))
+
+            bodyPoints= [f(kkkk) for kkkk in range(33)]
+            ##############################################
+
+        '''
+            로봇 감지 코드
             
+            반환될 변수:
+            robotDetected(bool)
+            robotPos(tuple)
+        '''
+
+        if len(bodyPoints) != 0:
+            ###############옆구리 계산 및 표시 ##################
+            arr= [bodyPoints[11], bodyPoints[23]]
+            d= ((arr[0][0]+arr[1][0])/2, (arr[0][1]+arr[1][1])/2)
+            cv2.circle(frame, d, 10, (255, 125, 0), -1)
+            ##################################################
             
-            arr= [f(11), f(23)]
-            d= ((arr[0][0]+arr[1][0])/2, (arr[0][1]+arr[1][1])/2) 
-            cv2.putText(frame, "right : " + str(d[0]) + " , " + str(d[1]), 
-                        (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            #blue_pts.append(d)
-            #blue_pts.append(arr[0])
-            #blue_pts.append(arr[1])
-            
-            import math
-            if matrix is not None:
-                x1, y1 = get3Dpos(matrix, arr[0])
-                x2, y2 = get3Dpos(matrix, arr[1])
-                x3, y3 = (x1+x2)/2, (y1+y2)/2
-                
-                blue_pts.append(transform_to_screen(matrix_inv, (x1,y1)))
-                blue_pts.append(transform_to_screen(matrix_inv, (x2,y2)))
-                blue_pts.append(transform_to_screen(matrix_inv, (x3,y3)))
-                
-                for i in range(360):
-                    x4, y4= math.cos((i*math.pi)/180) * 500, math.sin((i*math.pi)/180) * 500
-                    d2= transform_to_screen(matrix_inv, (x4+x3, y4+y3))
-                    blue_pts.append(d2)
-            
-        
-        if matrix is not None:
-            for bp in blue_pts:
-                x3d, y3d = get3Dpos(matrix, bp)
-                bp= tuple(map(int, bp))
-                cv2.circle(frame, bp, 10, (255, 0, 0), -1)
-                #cv2.putText(frame, f"3D: ({x3d:.0f}, {y3d:.0f})", (bp[0]+10, bp[1]), 
-                #            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-            
-            
-            #cv2.circle(frame, d2, radius=5, color=(0, 0, 255), thickness=-1)
-            
-            
+            if matrix is not None and True:#robotDetected:
+                tmpPList= [get3Dpos(matrix, bodyPoints[kkkk]) for kkkk in range(33)]
+                ressss= get_transformed_screen_vertices(matrix, w, h)
+                lx= abs(ressss['min_max_limits'][0]-ressss['min_max_limits'][2])
+                ly= abs(ressss['min_max_limits'][1]-ressss['min_max_limits'][3])
+                route= find_robot_path(tmpPList, mp_pose.POSE_CONNECTIONS, (lx,ly), lx, ly, 10, 30) #robotPos
+
+        if len(route) != 0 and True:#robotDetected:
+            pass
+            # 대충 로봇 조종
+
 
         # 결과 화면 출력
         cv2.imshow('Pose Detection1', frame)
         try:
-            if matrix is not None:
+            if matrix is not None and matrix_inv is not None:
+                if len(route) != 0:
+                   aaRoute= [transform_to_screen(matrix_inv, pptt) for pptt in route]
+                   frame = draw_robot_path(frame, aaRoute) 
                 ff= warp_perspective_no_crop(frame, matrix)
                 cv2.imshow('Pose Detection2', ff)
-        except:
-            print("e")
+        except Exception as e:
+            print(f"Error: {e}")
         
         # 'q'를 누르면 종료
         if cv2.waitKey(1) == ord('q'):
@@ -193,3 +199,34 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 cap.release()
 cv2.destroyAllWindows()
 
+
+
+
+
+# import math
+            # if matrix is not None:
+            #     x1, y1 = get3Dpos(matrix, arr[0])
+            #     x2, y2 = get3Dpos(matrix, arr[1])
+            #     x3, y3 = (x1+x2)/2, (y1+y2)/2
+                
+            #     blue_pts.append(transform_to_screen(matrix_inv, (x1,y1)))
+            #     blue_pts.append(transform_to_screen(matrix_inv, (x2,y2)))
+            #     blue_pts.append(transform_to_screen(matrix_inv, (x3,y3)))
+                
+            #     for i in range(360):
+            #         x4, y4= math.cos((i*math.pi)/180) * 500, math.sin((i*math.pi)/180) * 500
+            #         d2= transform_to_screen(matrix_inv, (x4+x3, y4+y3))
+            #         blue_pts.append(d2)
+            
+
+            # if matrix is not None:
+            #             for bp in blue_pts:
+            #                 # x3d, y3d = get3Dpos(matrix, bp)
+            #                 bp= tuple(map(int, bp))
+            #                 cv2.circle(frame, bp, 10, (255, 0, 0), -1)
+            #                 #cv2.putText(frame, f"3D: ({x3d:.0f}, {y3d:.0f})", (bp[0]+10, bp[1]), 
+            #                 #            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                        
+                        
+            #             #cv2.circle(frame, d2, radius=5, color=(0, 0, 255), thickness=-1)
+                     
